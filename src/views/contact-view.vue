f504f2d6050ab15f32df3ec93c716ddbff7055ed
OriolDeprius <156204782+OriolDeprius@users.noreply.github.com>
2025-03-26 16:58:56 +0100

    added linkedIn on contact

--- src/views/contact-view.vue
+++ src/views/contact-view.vue
@@ -7,12 +7,17 @@
         <div class="flex flex-col justify-center items-center h-full">
             <h1 class="text-4xl font-bold text-center">Contact Me</h1>
             <p class="mt-4 text-lg text-center">Feel free to reach out to me via email!</p>
-            <a
-                href="mailto:orioldeprius@gmail.com?subject=Contact%20from%20Portfolio&body=Hi%20there,%0D%0A%0D%0AI%20would%20like%20to%20get%20in%20touch%20with%20you."
-                class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >
-                Send me an Email
-            </a>
+            <p class="mt-4 text-lg text-center">Or connect with me on LinkedIn!</p>
+            <div class="flex flex-col sm:flex-row gap-5 mt-6">
+                <a href="mailto:orioldeprius@gmail.com?subject=Contact%20from%20Portfolio&body=Hi%20there,%0D%0A%0D%0AI%20would%20like%20to%20get%20in%20touch%20with%20you."
+                    class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    Send me an Email
+                </a>
+                <a href="https://www.linkedin.com/in/orioldepriusduixans/" target="_blank" rel="noopener noreferrer"
+                    class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    Visit my LinkedIn
+                </a>
+            </div>
         </div>
     </section>
 </template>
