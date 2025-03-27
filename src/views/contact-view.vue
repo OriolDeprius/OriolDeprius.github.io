c978c2d672097e4d77209932f3b48351d1a9ef3a
OriolDeprius <156204782+OriolDeprius@users.noreply.github.com>
2025-03-27 13:17:42 +0100

    contact change

--- src/views/contact-view.vue
+++ src/views/contact-view.vue
@@ -3,20 +3,20 @@
 </script>
 
 <template>
-    <section id="contact" class="h-screen mt-[2%] backdrop-blur-md container mx-auto" data-aos="zoom-in-up" data-aos-duration="1000">
+    <section id="contact" class="mt-[2%] mb-[2%] backdrop-blur-md container mx-auto" data-aos="zoom-in-up" data-aos-duration="1000">
         <div class="flex flex-col justify-center items-center h-full">
             <div class="border w-[90%] mt-[2%]"></div>
             <h1 class="text-4xl font-bold text-center">Contact Me</h1>
             <div class="border w-[90%] mb-[2%]"></div>
             <p class="mt-4 text-lg text-center">Feel free to reach out to me via email!</p>
             <p class="mt-4 text-lg text-center">Or connect with me on LinkedIn!</p>
-            <div class="flex flex-col sm:flex-row gap-5 mt-6">
+            <div class="flex flex-col sm:flex-row gap-5 mt-6 mb-6">
                 <a href="mailto:orioldeprius@gmail.com?subject=Contact%20from%20Portfolio&body=Hi%20there,%0D%0A%0D%0AI%20would%20like%20to%20get%20in%20touch%20with%20you."
-                    class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">
                     Send me an Email
                 </a>
                 <a href="https://www.linkedin.com/in/orioldepriusduixans/" target="_blank" rel="noopener noreferrer"
-                    class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300">
                     Visit my LinkedIn
                 </a>
             </div>
